6c33ef6997707a9878114f4ffc721d6f2468818f
Dongchen Han <dongchen_han@yahoo.com>
2026-05-19 15:42:01 -0700

    Add Blazor Server UI

---
+++ FlowerBlazorServerUI/Data/FlowerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowerBlazorServerUI.Data
+{
+    public class FlowerModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Type { get; set; }
+        public required string Description { get; set; }
+        public required string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
